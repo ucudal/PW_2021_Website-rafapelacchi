9a294ee631d3576c4b8b1f34598ff71ea6043a73
RafaPelacchi <rafael.pelacchi@gmail.com>
2021-11-14 14:23:25 -0300

    preparo experiencia para consumo de api

---
+++ docs/servidor.js
@@ -0,0 +1,22 @@
+
+var experiencia_laboral = [
+  {
+    empresa: "Urudata",
+    puesto: "Developer",
+    descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nost                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nost                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nost                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nost                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nost                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nost                                                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nost                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nost                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, or incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nost.",
+    fechaInicio: new Date("2019-5-19"),
+    fechaFin: new Date("2021-12-03"),
+  },
+  {
+    empresa: "Datasec",
+    puesto: "Developer",
+    descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nost                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nost                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nost                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nost                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nost                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nost                                                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nost                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nost                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, or incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nost.",
+    fechaInicio: new Date("2016-6-20"),
+    fechaFin: new Date("2019-5-19"),
+  }
+]
+
+
+for(var i = 0; i < experiencia_laboral.length; i++){
+  agregarExperiencia(experiencia_laboral[i])
+}
--- src/js/experiencia.js
+++ src/js/experiencia.js
@@ -1,35 +1,36 @@
-var experienciaUrudata = document.getElementById('urudata');
-var experienciaDatasec = document.getElementById('datasec');
 var flechaSiguiente = document.getElementById('flechaSiguiente');
 var flechaAntes = document.getElementById('flechaAntes');
-var experienciaActual= "urudata";
+var experienciaActual= 1;
 
-flechaSiguiente.addEventListener("click", cambiarExperiencia)
-flechaAntes.addEventListener("click", cambiarExperiencia) 
+flechaSiguiente.addEventListener("click", aumentarExperiencia);
+flechaAntes.addEventListener("click", disminuirExperiencia);
 
-var aprobarMateria = document.getElementById('aprobarMateria');
-aprobarMateria.addEventListener("click", aprobarCurso)
+
+function aumentarExperiencia(){ 
+    experienciaActual++;
+    cambiarExperiencia();
+}
+function disminuirExperiencia(){
+    experienciaActual--;
+    cambiarExperiencia()
+}
 
 function cambiarExperiencia(){
-    switch(experienciaActual) 
-    {
-        case "datasec": {
-            flechaSiguiente.style.display = 'block';
-            flechaAntes.style.display = 'none';
-            experienciaUrudata.style.display = 'block';
-            experienciaDatasec.style.display = 'none';
-            experienciaActual= "urudata";
-            break;
+    var elementos = document.getElementsByClassName('experiencia');
+
+    for (var i = 0; i < elementos.length; i++) {
+        var aux = elementos[i];
+
+        if(aux.dataset.indice == experienciaActual)
+        { 
+            aux.style.display = "block";
         }
-        case "urudata": {
-            flechaSiguiente.style.display = 'none';
-            flechaAntes.style.display = 'block';
-            experienciaUrudata.style.display = 'none';
-            experienciaDatasec.style.display = 'block';
-            experienciaActual= "datasec";
-            break;
-        } 
-    }
+        else
+            aux.style.display = "none";
+     }
+ 
+    flechaAntes.style.display = experienciaActual == 1 ? "none" : "block";
+    flechaSiguiente.style.display = experienciaActual == elementos.length ? "none" : "block";
 }
 
 function aprobarCurso(){
@@ -42,4 +43,40 @@ function aprobarCurso(){
 
     var tercerRegla = document.getElementById('tercerRegla');
     tercerRegla.style.display = "block";
+}
+
+var aprobarMateria = document.getElementById('aprobarMateria');
+aprobarMateria.addEventListener("click", aprobarCurso)
+
+function agregarExperiencia(aux){
+
+var contenedorExperiencia = document.getElementById("contenedorExperiencia");
+
+  var divExperiencia = document.createElement("div");
+  divExperiencia.className="bg-white p-16 experiencia";
+  if(i!=0)
+    divExperiencia.style.display = "none";
+
+  var header = document.createElement("div");
+  header.className="text-center";
+
+  var labelExperiencia = document.createElement("label");
+  labelExperiencia.className="font-extrabold text-3xl";
+  labelExperiencia.textContent = aux.empresa;
+
+  var divFecha = document.createElement("div");
+  divFecha.className="text-center pb-7 text-gray-400";
+  divFecha.textContent =  "( " + aux.fechaInicio.getDay() + "/"+ aux.fechaInicio.getMonth()+ "/"+ aux.fechaInicio.getFullYear() + " - "+ aux.fechaFin.getDay() + "/"+ aux.fechaFin.getMonth()+ "/"+ aux.fechaFin.getFullYear() + " )";
+
+  var body = document.createElement("div");
+  body.textContent = aux.descripcion;
+
+  header.appendChild(labelExperiencia);
+  divExperiencia.appendChild(header);
+  divExperiencia.appendChild(divFecha);
+  divExperiencia.appendChild(body);
+
+  divExperiencia.dataset.indice = i+1;
+
+  contenedorExperiencia.appendChild(divExperiencia)
 }
