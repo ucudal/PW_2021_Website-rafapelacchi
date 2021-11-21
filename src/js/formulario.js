13ccaa0c7b5a8cd7e1e96d239bd9e91e88d8acec
RafaPelacchi <rafael.pelacchi@gmail.com>
2021-11-21 19:21:42 -0300

    agrego ts obligatorio

--- src/js/formulario.js
+++ src/js/formulario.js
@@ -1,61 +1,15 @@
-var usuarios = [];
-
-class Usuario{
-    constructor(nombre, mail, comentario) {
-        this.nombre = nombre;
-        this.mail = mail;
-        this.comentario = comentario;
-        this.erroresValidacion = [];
-        this.valido = false;
-    };
-
-    getName(){
-        return this.nombre;
-    }
-
-    esValido(){
-        if(!this.nombre)
-            this.erroresValidacion.push("Usted no ha ingresado un nombre.");
- 
-        if(!this.mail)
-            this.erroresValidacion.push("Usted no ha ingresado un mail.")
-        else if( this.mail.indexOf("@") < 0 || this.mail.indexOf(".") < 0)
-            this.erroresValidacion.push("El mail ingresado no es correcto.")
-
-        if(!this.comentario)
-            this.erroresValidacion.push("Usted no ha ingresado un comentario.")
-    
-        this.valido = !(this.erroresValidacion.length > 0) 
-
-        return this.valido;
-    }
-  } 
- 
-  document.getElementById('registrarUsuario').addEventListener("click", function (event) {
+document.getElementById('registrarUsuario').addEventListener("click", function (event) {
     event.preventDefault();
-      let nombre = document.getElementById('nombre').value; 
-      let mail = document.getElementById('mail').value;
-      let comentario = document.getElementById('comentario').value;
 
-      let usuarioAuxiliar = new Usuario(nombre,mail,comentario); 
+    let nombre = document.getElementById('nombre').value; 
+    let mail = document.getElementById('mail').value;
+    let comentario = document.getElementById('comentario').value;
 
-      if(usuarioAuxiliar.esValido())
-      {
-        usuarios.push(usuarioAuxiliar);
-        limpiarFormulario()
-        generarCookie(usuarioAuxiliar)
-      }
-      else
-      {
-        usuarioAuxiliar.erroresValidacion.forEach(function(element){
-            mostrarMensaje(element, "error"); 
-        });
-      }
-
-  })
-
-function limpiarFormulario(){
-    document.getElementById('nombre').value = ''; 
-    document.getElementById('mail').value = ''; 
-    document.getElementById('comentario').value = ''; 
-}
+    var result = impactarFormulario(nombre, mail, comentario);
+    if(result == 0)
+    {
+        document.getElementById('nombre').value = ''; 
+        document.getElementById('mail').value = ''; 
+        document.getElementById('comentario').value = ''; 
+    }
+})
