c8171612c6b0f95430c399fbeec3d96ea3094e54
RafaPelacchi <rafael.pelacchi@gmail.com>
2021-11-23 17:37:14 -0300

    elimino codigo basura

--- docs/servidor.js
+++ docs/servidor.js
@@ -8,30 +8,3 @@ fetch('https://PW2021-APINode-rafapelacchi.rp33.repl.co/experiencia-laboral').th
     agregarExperiencia(json[i], i) 
   }
 })
-
-
-var user = {
-  nombre: "test",
-  mail: "test@sdf.com"
-};
-var url = "https://PW2021-APINode-rafapelacchi.rp33.repl.co/enviar-formulario";
-fetch(url, {
-  method: "POST",
-  body: JSON.stringify(user),
-  credentials: "same-origin",
-  headers: { 
-      cache: 'no-store',
-      'Content-Type': 'application/json',
-      withCredentials:true
-  }
-}).then(function (response) {  
-  console.log(response)
-  console.log(response.headers)
-  console.log(response.headers["set-cookie"])
-  if(response.status == 303)
-       mostrarMensaje(response.text.toString(), "error");
-  else
-       mostrarMensaje(user.nombre + ", a la brevedad nos contactaremos con usted.", "success")
-})["catch"](function (error) {
-  console.error(error);
-}); 
